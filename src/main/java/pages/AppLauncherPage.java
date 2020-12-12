05082cac3388a6da8d30f1d51bfe9a3b56445b2f
NivedaRN <raniniveda.aug31@gmail.com>
2020-12-12 10:11:17 -0800

    Assessment code

--- src/main/java/pages/AppLauncherPage.java
+++ src/main/java/pages/AppLauncherPage.java
@@ -6,32 +6,18 @@ import com.aventstack.extentreports.ExtentTest;
 
 import lib.selenium.PreAndPost;
 
-public class AppLauncherPage extends PreAndPost {
+public class AppLauncherPage extends PreAndPost{
 
-	public AppLauncherPage(EventFiringWebDriver driver, ExtentTest test) {
-		this.driver = driver;
-		this.test = test;
-	}
-
-	public ServicePage clickService() {
-		click(locateElement("link", "Service"));
-		return new ServicePage(driver, test);
-	}
-
-	
-	public HomePage clickSales()
+	public AppLauncherPage(EventFiringWebDriver driver, ExtentTest test)
 	{
-		click(locateElement("xpath","//p[text()='Sales']"));
-		return new HomePage(driver,test);
+		this.driver=driver;
+		this.test=test;
 	}
-
-
-	public CMSPage clickCMS() {
-		String locValue = "//span[@class='slds-truncate label-display']//p[text()='CMS Home']";
-		scrollToElement(locateElement("xpath", locValue));
-		click(locateElement("xpath", locValue));
-		return new CMSPage(driver, test);
-
+	
+	public ServiceConsolePage clickServiceConsole()
+	{
+		click(locateElement("xpath","//p[text()='Service Console']"));
+		return new ServiceConsolePage(driver, test);
+		
 	}
-
 }
